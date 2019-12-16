4a61524f77a18d4a8457094cf8eb518c7f3b7fed
Tola's Terminal <sohbaker@gmail.com>
2019-12-16 16:47:41 +0000

    WIP: allows a user to add a new TODO

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -2,4 +2,15 @@ describe("To Do List Application", () => {
     it("displays the home page", () => {
       cy.visit("/").contains("Welcome to To Do List");
     });
+
+    it ("allows a user to create a new TODO", () => {
+        cy.visit("/");
+
+        cy
+            .get('input[name="todo"]')
+            .type("drink water")
+            .should("have.value", "drink water");
+
+        cy.get("form").submit();
+    });
 });
--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,29 @@ module Main exposing (main)
 
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 
+
+type alias Model =
+    { entries : List Entry
+    , uuid : Int
+    }
+
+type alias Entry =
+    { name: String
+    , completed: Bool
+    , id : Int
+    }
 
 main =
     div []
         [ h1 [] [ text "Welcome to To Do List" ]
+        , Html.form []
+            [ label []
+                 [  text "What needs to be done?"
+                 , input [type_ "text", name "todo" ] []
+                 ]
+            ]
         ]
+
+
