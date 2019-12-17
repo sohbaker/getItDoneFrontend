ccddff9cb070518c10331f8309bf04643eda03af
Tola's Terminal <sohbaker@gmail.com>
2019-12-17 15:34:31 +0000

    passes: allows a user to create a new TODO

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -2,4 +2,18 @@ describe("To Do List Application", () => {
     it("displays the home page", () => {
       cy.visit("/").contains("Welcome to To Do List");
     });
+
+    it ("allows a user to create a new TODO", () => {
+        cy.visit("/");
+
+        cy
+            .get('input[name="todo"]')
+            .type("drink water")
+            .should("have.value", "drink water");
+
+        cy.get('button').click();
+
+        cy.get(".all-entries")
+            .should("contain", "drink water");
+    });
 });
