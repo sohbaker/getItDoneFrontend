433b3fa9012e87d8fb46903fd059f2b8175a4772
Tola's Terminal <sohbaker@gmail.com>
2020-01-09 12:11:08 +0000

    begins custom cypress command to reset backend before each test

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -1,48 +1,53 @@
 describe('To Do List Application', () => {
+    // beforeEach( () => {
+    //     cy.resetBackend();
+    //     // cy.visit("/teardown");
+    // });
+
     context('When the home page is first opened', () => {
         it('displays the welcome message', () => {
             cy.visit('/').contains('To Do List');
         });
 
         it('focuses on the input field for a new todo', () => {
-            cy.visit('/')
+            cy.visit('/');
             cy
                 .get('.new-todo-item').focus()
                 .should('have.class', 'new-todo-item')
         });
-    })
+    });
 
     context('Fetching TODOs', () => {
         it ("displays current todo items", () => {
-            cy.visit('/')
+            cy.visit('/');
 
             cy.get('.todo-list').find('tr').should('have.length', 4)
         });
-    })
+    });
 
     context('Creating TODOs', () => {
-       it ("allows a user to create multiple TODOs", () => {
-            cy.visit('/')
+        it ("allows a user to create multiple TODOs", () => {
+            cy.visit('/');
 
             cy
                 .get('.new-todo-item')
                 .type('sip cocktail')
-                .should('have.value', 'sip cocktail')
+                .should('have.value', 'sip cocktail');
 
-            cy.get('button').click()
+            cy.get('button').click();
 
             cy.get('.todo-list td')
-                .should('contain', 'sip cocktail')
+                .should('contain', 'sip cocktail');
 
             cy
                 .get('.new-todo-item')
                 .type('go for a walk')
-                .should('have.value', 'go for a walk')
+                .should('have.value', 'go for a walk');
 
-            cy.get('button').click()
+            cy.get('button').click();
 
             cy.get('.todo-list td')
                 .should('contain', 'go for a walk')
-       });
-    })
+        });
+    });
 });
--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
 import { Elm } from '../src/Main.elm'
+import '../cypress/support/commands'
 
 Elm.Main.init({ node: document.getElementById("app") })
